c0215b0cd8ccfb03e81d34f190988916bf8245e8
rahmamohamed-29 <rahmamohamed.refai@gmail.com>
2026-09-18 02:44:02 +0300

    Add constructor to AppDbContext
    Added constructor to AppDbContext for dependency injection.

--- SmartMart/Data/AppDbContext.cs
+++ SmartMart/Data/AppDbContext.cs
@@ -5,17 +5,17 @@ namespace SmartMart.Data
 {
     public class AppDbContext : DbContext
     {
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options)
+{
+}
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<Product> Products { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            
-        }
+    
         override protected void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
